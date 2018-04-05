cfe339b8a6803816bdcfbc8d1ca9faddf34a3f11
anhngk <nguyenkyanh1408@gmail.com>
2018-04-05 18:25:04 +0700

    Note 5/4

--- 1.1	Git.rb
+++ 1.1	Git.rb
@@ -1,1 +1,17 @@
 - Config remote URL : git config remote.<remote-name>.url
+
+- Replace local changes : git checkout --<file-name> 
+
+- Git rebase : Đường đi của nhánh - Đường thẳng
+- Git merge  : Đường đi của nhánh - Phân nhánh
+
+- 2 người làm việc trên 1 branch : Thời điểm 1 : Người A chuẩn bị push code - Người B push code mới lên
+							-> Người A phải 'git pull' -> solve conflict (nếu có) -> commit -> push
+
+- 'git push' : Push tất cả những thay đổi ở local repo lên Github
+- 'git pull' : Chỉ pull những thay đổi từ branch hiện tại
+
+- Branch : master | login
+	+ (login) pull code từ branch master ở remote origin : git pull origin master
+	+ (login) pull code từ branch master ở local repo : git pull origin/master
+	+ (login) push thẳng từ branch login lên branch master ở remote : git push origin login:master				
---
+++ 2.2	CSS.rb
@@ -0,0 +1,48 @@
+CSS Selector :
+-	elementA + elementB : Select elementB ngay sau thẻ đóng elementA
+-	elementA elementB : Select tất cả elementB nằm trong elementA
+-	elementA > elementB : Select tất cả elementB được bao trực tiếp (parent) bởi elementA
+-	elementA ~ elementB : Select tất cả elementB nằm dưới elementA trong tập HTML
+-	element[attributeA]: Select các element có attributeA
+-	[attribute ~= valueA] : Select các element có attribute chứa valueA
+-	[attribute |= valueA] : Select các element có gía trị bắt đầu bằng valueA
+-	[attribute ^= valueA] : 
+-	[attribute $= valueA] : Select các element có gía trị kết thúc bằng valueA
+
+:active
+:checked
+:disabled
+:empty element that has no children
+:enabled
+:first-child
+:first-of-type
+:focus
+:hover
+:in-range
+:invalid
+:lang
+:last-child
+:last-of-type
+:link
+:not(selector)
+:nth-child(n)
+:nth-last-child(n)
+:nth-last-of-type(n)
+:nth-of-type(n)
+:only-of-type
+:only-child
+:optional
+:out-of-range
+:read-only
+:read-write
+:required
+:root
+:target
+:valid
+:visited
+
+::after
+::before
+::first-letter
+::first-line
+::selection
