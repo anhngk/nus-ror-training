7ec4808fa8f08f47af048629d335f36bce54648b
anhngk <nguyenkyanh1408@gmail.com>
2018-04-06 17:49:55 +0700

    Note 06/04/2018

--- 1.1	Git.rb
+++ 1.1	Git.rb
@@ -11,7 +11,17 @@
 - 'git push' : Push tất cả những thay đổi ở local repo lên Github
 - 'git pull' : Chỉ pull những thay đổi từ branch hiện tại
 
-- Branch : master | login
+- Context: branches(master, login), standing on branch login	
 	+ (login) pull code từ branch master ở remote origin : git pull origin master
-	+ (login) pull code từ branch master ở local repo : git pull origin/master
-	+ (login) push thẳng từ branch login lên branch master ở remote : git push origin login:master				
+	+ (login) merge code từ branch master ở local repo : git merge master
+	+ (login) push thẳng từ branch login lên branch master ở remote : git push origin login:master
+	+ git pull origin master = git fetch origin + git merge origin/master
+
+- Unstaging files : git reset HEAD <file-name>
+- Undoing a commit : git reset --soft HEAD^ (HEAD^^ - undo last 2 commit)
+- Adding to a commit : git add ... git commit --amend -m "message"
+
+- git remote add <remote-name> <address>
+- git remote rm <name>
+
+
---
+++ 2.3-4-5	CSS-JS-jQuery.rb
@@ -0,0 +1,51 @@
+HOMEWORK
+- Lấy chữ số input
+- Truyền vào hàm eval()
+- Có kết quả dùng jQuery show ra
+
+Requirement
+- Button : Hover đổi màu - Press lún xuống - Unpress nhả ra
+- Layout mobile : Hiển thị 2 cột button theo thứ tự từ 0 - 9 + - x : = clear
+- Lịch sử tính toán : Dưới ô hiển thị kết quả - Dropdown - Chọn history record -> Tính + Hiển thị kết quả
+- Clear button : Show modal confirm Clear or not
+- Ô kết quả khi đã tính toán - Tính 1 phép tính mới, clear màn hình - Show phép tính mới
+- Toggle button : Active -> Show 4 button sin cos tan cotg
+
+----------------------------------
+ Aware of Syntax - Variables - Data Types - OOP when approaching a new programming language.
+----------------------------------
+
+	2.3 Javascript
+
+-	Can be place in <body> <head>
+-	<script></script>
+-	Using document.write() after an HTML document is fully loaded, will delete all existing HTML
+-	Case sensitive : lastName vs lastname -> different variables
+-	var x = "5" + 2 + 3 -> "523" / var x = 2 + 3 + "5" -> "55"
+-	100 ** 3 == 100^3
+-	Data type of 'null' is an object - 'null' is nothing 
+	typeof undefined           // undefined
+	typeof null                // object
+
+	null === undefined         // false
+	null == undefined          // true
+
+-	typeof [array] -> object
+-	typeof function -> function
+-	Scope : If you assign a value to a variable that has not been declared, it will automatically become a GLOBAL variables
+-	slice(from,to) / substr(from,length)
+-	String methods : split() / charAt() / charCodeAt() / concat() / toUpper(Lower)Case() / replace() / substr() / slice() / search() / indexOf() / lastIndexOf() / length()
+-	typeof NaN -> number
+-	Infinity
+-	Number methods : toString() / toExponential() / toFixed() / toPresision() / valueOf() / Number() / parseInt() / parseFloat() / 
+-	Math.PI/.round()/.pow()/.sqrt()/.abs()/.ceil()/.floor()/.sin()/.cos()/.tan()/.min()/.max()/.random()
+-	Math.random() return a ramdon number 0 -> 1 (exclusive)
+-	Date methods : toUTCString() / toDateString()
+-	Adding array elements (2 ways): fruits.push("Lemon") / fruits[fruits.length] = "Lemon"
+-	Difference between Array & Object : array -> numbered indexes / object -> named indexes
+-	Array methods : toString() / join('separate symbol') / pop() / shift() (opposite to pop()) / unshift() / splice() / concat() - merging 2 array / sort() / resverse()
+-	Compare function : function(a,b){ return a-b}
+-	Array Sort
+-	5 Datatype contain values : string/number/boolean/object/function
+-	3 types of objects : Object/Date/Array
+-	2 Datatype can not contain data : null / undefined 
