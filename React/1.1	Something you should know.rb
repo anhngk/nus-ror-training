e35d90dda250c543092251f971960785a2b4e689
anhnk <anhnk@nustechnology.com>
2018-06-04 17:22:57 +0700

    Update React training

---
+++ React/1.1	Something you should know.rb
@@ -0,0 +1,6 @@
+- Difference between let & var :
+  * let: declare block-level variables and only available inside block it is enclosed in
+  * var: declare variables within function
+
+- Arrow function syntax
+- Spread syntax (operator)
---
+++ React/Demo/app.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+  class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+
+    speak() {
+      console.log(this.name + ' makes a noise.');
+    }
+  }
+
+  class Dog extends Animal {
+    constructor(name) {
+      super(name);
+    }
+
+    speak() {
+      console.log(this.name + ' barks.');
+    }
+  }
+
+  var dog = new Dog("Lucky");
+  dog.speak();
+});
